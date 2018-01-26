f8da41a04d3ee7226e7738c54e9e8d0fc24579ad
Bhavani Shankar <ebhavanishankar@gmail.com>
2018-01-26 15:09:10 +0530

    implement choose partially

--- src/player.js
+++ src/player.js
@@ -4,8 +4,25 @@ class Player {
     this.hand = []
   }
 
-  choose (top) {
-    return this.hand[0]
+  choose (topCard, color, drawCount) {
+
+    if (drawCount !== undefined) {
+      // play draw equal or better card
+    }
+
+    if (color !== undefined) {
+      for (let i = 0; i < this.hand.length; ++i) {
+        if (color === this.hand[i].color) return this.hand[i]
+      }
+    }
+
+    for (let i = 0; i < this.hand.length; ++i) {
+      if (topCard.color === this.hand[i].color ||
+          this.hand[i] === 'anycolor' ||
+          topCard.value === this.hand[i].value) {
+        return this.hand[i]
+      }
+    }
   }
 }
 
