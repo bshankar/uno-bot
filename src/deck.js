683b2296bff6ab663e7fd0aa6d9b9b1bf435f4c9
Bhavani Shankar <ebhavanishankar@gmail.com>
2018-01-26 13:21:08 +0530

    implement deal

--- src/deck.js
+++ src/deck.js
@@ -1,3 +1,5 @@
+const Card = require('./card')
+
 class Deck {
   constructor () {
     // cards []
@@ -5,7 +7,10 @@ class Deck {
   }
 
   deal (players) {
-
+    for (let i = 0; i < players.length; ++i) {
+      players.hand.push(this.cards.slice(7 * i, 7 * i + 6))
+    }
+    this.remaining = players.slice(7 * players.length)
   }
 
   add (card) {
