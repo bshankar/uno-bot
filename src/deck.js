35374c3c31c729503990f114b05ef0385bb8ef97
saumya-singh <saumya.singh0993@gmail.com>
2018-01-26 13:49:03 +0530

    fixed error

--- src/deck.js
+++ src/deck.js
@@ -5,31 +5,34 @@ class Deck {
   }
 
   card_init () {
-    cards = {}
+    this.cards = {}
     var i, j
     var colors = ["red", "blue", "yellow", "green"]
     var remvalues = ["+2", "reverse", "skip"]
+    var wildcards = ["+4", "colorChange"]
     for (k = 0; k <2; k++){
       for ( i = 1; i <10; i ++){
         for (j = 0; j < 4; j++){
-          cards["color"] = colors[j]
-          cards["value"] = i + ""
+          this.cards["color"] = colors[j]
+          this.cards["value"] = i + ""
         }
       }
       for ( i = 0; i < 4; i ++){
         for (j = 0; j < 3; j++){
-          cards["color"] = colors[j]
-          cards["value"] = remvalues[i]
+          this.cards["color"] = colors[j]
+          this.cards["value"] = remvalues[i]
         }
       }
     }
     for ( i = 0; i < 4; i ++) {
-      cards["color"] = colors[i]
-      cards["value"] = "0"
+      this.cards["color"] = colors[i]
+      this.cards["value"] = "0"
     }
-    for (i = 0; i < 4; i++) {
-      cards["color"] = colors[i]
-      cards["value"] = "anycolor"
+    for ( i = 0; i < 4; i ++){
+      for (j = 0; j < 2; j++){
+        this.cards["color"] = "anycolor"
+        this.cards["value"] = wildcards[i]
+      }
     }
     console.log(cards)
   }
