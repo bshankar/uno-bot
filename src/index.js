911c7337261ebf2a7ae2a69b975e802c8d9a8ca7
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-26 21:00:23 +0530

    Add more tests

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const {Card} = require('./card')
 // const d = new Deck()
 // console.log(d.cards.length)
 
-// d.add(d.cards[0])
+// d.add(d.cards[g.currentPlayer])
 // d.add(d.cards[1])
 // d.add(d.cards[2])
 // console.log(d.remaining)
@@ -13,32 +13,53 @@ const {Card} = require('./card')
 // d.draw(2)
 // console.log(d.remaining)
 
-const g = new Game([1, 2, 3, 4])
+const g = new Game(['P1', 'P2', 'P3', 'P4'])
 g.start()
+
 console.log(g.currentPlayer)
-g.players[g.currentPlayer].hand = [new Card('red', '+2')]
+console.log(g.players[0].name)
+g.players[0].hand = [new Card('red', '+2')]
+console.log(g.players[0].hand.length)
 g.play()
+console.log('After playing')
+
+console.log(g.currentPlayer)
+console.log(g.players[1].name)
+console.log(g.players[1].hand.length)
 console.log(g.top)
-console.log(g.currentPlayer)
-g.players[g.currentPlayer].hand = [new Card('green', '+2')]
+g.players[1].hand = [new Card('green', '+2')]
+console.log(g.players[1].hand.length)
 g.play()
+console.log('After playing')
+
+console.log(g.currentPlayer)
+console.log(g.players[2].name)
+console.log(g.players[2].hand.length)
 console.log(g.top)
-console.log(g.currentPlayer)
-g.players[g.currentPlayer].hand = [new Card('anycolor', '+4')]
+g.players[2].hand = [new Card('anycolor', '+4')]
+console.log(g.players[2].hand.length)
 g.play()
+console.log('After playing')
+
+console.log(g.currentPlayer)
+console.log(g.players[3].name)
+console.log(g.players[3].hand.length)
 console.log(g.top)
-console.log(g.currentPlayer)
 g.play()
+console.log('After playing')
+
+console.log(g.currentPlayer)
+console.log(g.players[g.currentPlayer].name)
 console.log(g.players[g.currentPlayer].hand.length)
 
-// console.log(g.currentPlayer)
+// console.log(g.currentPlayerer)
 
 // g.playCard(new Card('green', 'skip'))
-// console.log(g.currentPlayer)
+// console.log(g.currentPlayerer)
 // g.playCard(new Card('green', 'reverse'))
-// console.log(g.players[g.currentPlayer])
+// console.log(g.players[g.currentPlayerer])
 // g.play()
-// console.log(g.currentPlayer)
-// console.log(g.players[0].hand.length)
+// console.log(g.currentPlayerer)
+// console.log(g.players[g.currentPlayer].hand.length)
 // g.playCard(new Card('green', '+2'))
-// console.log(g.players[0].hand.length)
+// console.log(g.players[g.currentPlayer].hand.length)
