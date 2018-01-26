5e86c9732b847274cf656ad16c629c757cfbb428
Bhavani Shankar <ebhavanishankar@gmail.com>
2018-01-26 14:33:19 +0530

    splice the hand of a player

--- src/game.js
+++ src/game.js
@@ -28,7 +28,7 @@ class Game {
   playCard (card) {
     this.top = card
     const index = this.players[this.currentPlayer].hand.indexOf(card)
-    this.players[this.currentPlayer].splice(index, 1)
+    this.players[this.currentPlayer].hand.splice(index, 1)
     this.currentPlayer = (this.currentPlayer + 1) % this.players.length
   }
 }
